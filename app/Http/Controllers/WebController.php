b7f6482d29fe2def90f039246d96bacc89a17445
Andrey Dyugaev <deprimehell@gmail.com>
2023-02-26 21:19:08 +0700

    Update WebController.php

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,12 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use App\Models\{
-  User,
-  Task,
-  Message,
-};
-
 class WebController extends Controller
 {
   public function index(Request $request)
